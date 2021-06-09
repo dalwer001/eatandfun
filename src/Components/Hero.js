ec42e9da1a54b78c73b342b39ef60566bcdfb10c
dalwer001 <dalwer.official@gmail.com>
2021-06-09 15:18:45 +0600

    add all

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Hero = () => {
     return (
         <div className="bg-white h-screen flex flex-col justify-center items-center">
-            <h1 className="lg:text-9xl md:text-7xl sm-text-5xl text-3xl font-black mb-14">
+            <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14">
                 Eat & Fun
             </h1>
             <Link className="py-6 px-10 bg-yellow-500 rounded-full text-2xl hover:bg-yellow-300 transition duration-300 ease-in-out flex items-center animate-bounce">Order Now 
